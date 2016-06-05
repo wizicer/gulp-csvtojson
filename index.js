225f0767834c06254b3b8f5ab86a618c5e98cca8
CHAPT023 <tim.chaplin@disney.com>
2016-06-05 19:35:52 +0100

    Updated to include streams

--- index.js
+++ index.js
@@ -1,54 +1,91 @@
 'use strict';
+var path = require('path');
 var gutil = require('gulp-util');
 var through = require('through2');
-var Converter=require("csvtojson").core.Converter;
-var path = require('path');
+var Converter=require("csvtojson").Converter;
 
+var getVariableName = function(file,options){
+    if (typeof(options.globalvariable) !== "undefined" && options.globalvariable !== null)
+        return options.globalvariable;
+
+    return path.basename(file.path, path.extname(file.path))
+};
+
+var onComplete = function(stream,file,options,done){
+    if (options.genjs){
+        file.path = gutil.replaceExtension(file.path,'.js');
+    }
+    else
+        file.path = gutil.replaceExtension(file.path,'.json');
+
+    stream.push(file);
+    return done();
+};
+
+var generateJsStream = function(variablename){
+    var firstLine = true;
+    return through(function(chunk,enc,cb){
+        if(firstLine){
+            this.push("var "+variablename+" = ");
+            firstLine=false;
+        }
+
+        this.push(chunk);
+        cb();
+    },function (cb) {
+        this.push(';');
+        cb();
+    });
+};
 
 module.exports = function (options) {
     if (typeof(options) === "undefined") {
         options = { genjs: false };
     }
-    // if (!options.foo) {
-    //     throw new gutil.PluginError('gulp-csvtojson', '`foo` required');
-    // }
 
     return through.obj(function (file, enc, cb) {
+        var self = this;
+
         if (file.isNull()) {
             cb(null, file);
             return;
         }
-
-        if (file.isStream()) {
-            cb(new gutil.PluginError('gulp-csvtojson', 'Streaming not supported'));
-            return;
-        }
-
+        
         try {
+            if (file.isStream()) {
+                options.constructResult=false;
+                options.toArrayString=true;
+                var csvConverter = new Converter(options);
 
-            var csvConverter = new Converter(options);
-            var gulpobj = this;
-            
-            csvConverter.fromString(file.contents.toString(), function(err, jsonObj) {
-                var output = JSON.stringify(jsonObj);
-                var variablename = path.basename(file.path, path.extname(file.path));
-                file.path = file.path.slice(0, file.path.indexOf('.')) + ".json"
-                if (typeof(options.globalvariable) !== "undefined" && options.globalvariable !== null) {
-                    variablename = options.globalvariable;
-                }
+                file.contents = file.contents.pipe(csvConverter);
                 if (options.genjs) {
-                    file.path = file.path.slice(0, file.path.indexOf('.')) + ".js"
-                    output = variablename + "=" + output + ";";
+                    var variablename = getVariableName(file,options)
+                    file.contents = file.contents.pipe(generateJsStream(variablename));
                 }
-                file.contents = new Buffer(output);
-                gulpobj.push(file);
-                cb();
-            });
+                return onComplete(self,file,options,cb);
+            }
 
+            if(file.isBuffer()){
+                var csvConverter = new Converter(options);
+                            
+                csvConverter.fromString(file.contents.toString(), function(err, jsonObj) {
+                    if(err){
+                        this.emit('error', new gutil.PluginError('gulp-csvtojson', err));
+                        cb(new gutil.PluginError('gulp-csvtojson', err));
+                    }
+
+                    var output = JSON.stringify(jsonObj);
+                    if (options.genjs) {
+                        var variablename = getVariableName(file,options)
+                        output = variablename + "=" + output + ";";
+                    }
+                    file.contents = new Buffer(output);
+                    onComplete(self,file,options,cb);
+                });
+            }
         } catch (err) {
             this.emit('error', new gutil.PluginError('gulp-csvtojson', err));
             cb(new gutil.PluginError('gulp-csvtojson', err));
         }
-
     });
 };
--- test.js
+++ test.js
@@ -2,85 +2,198 @@
 var assert = require('assert');
 var gutil = require('gulp-util');
 var csvtojson = require('./');
+var gulpCsvtojson = require('./');
+var File = require('vinyl');
+var Readable = require('stream').Readable;
+var concat = require('concat-stream');
 
 var input = "fieldA.title, fieldA.children[0].name, fieldA.children[0].id,fieldA.children[1].name, fieldA.children[1].employee[].name,fieldA.children[1].employee[].name, fieldA.address[],fieldA.address[], description\n" +
             "Food Factory, Oscar, 0023, Tikka, Tim, Joe, 3 Lame Road, Grantstown, A fresh new food factory\n" +
             "Kindom Garden, Ceil, 54, Pillow, Amst, Tom, 24 Shaker Street, HelloTown, Awesome castle";
 
-var expected = '[{"fieldA":{"title":"Food Factory","children":[{"name":"Oscar","id":"0023"},{"name":"Tikka","employee":[{"name":"Tim"},{"name":"Joe"}]}],"address":["3 Lame Road","Grantstown"]},"description":"A fresh new food factory"},{"fieldA":{"title":"Kindom Garden","children":[{"name":"Ceil","id":54},{"name":"Pillow","employee":[{"name":"Amst"},{"name":"Tom"}]}],"address":["24 Shaker Street","HelloTown"]},"description":"Awesome castle"}]';
+var expected = '[{"fieldA":{"title":"Food Factory","children":[{"name":"Oscar","id":23},{"name":"Tikka","employee":[{"name":"Tim"},{"name":"Joe"}]}],"address":["3 Lame Road","Grantstown"]},"description":"A fresh new food factory"},{"fieldA":{"title":"Kindom Garden","children":[{"name":"Ceil","id":54},{"name":"Pillow","employee":[{"name":"Amst"},{"name":"Tom"}]}],"address":["24 Shaker Street","HelloTown"]},"description":"Awesome castle"}]';
 
-it('should return json when no options', function (cb) {
-    var stream = csvtojson();
+var stringToStream = function(string){
+    var rs = new Readable();
+    rs.push(string);
+    rs.push(null);
+    
+    return rs;
+}
 
-    stream.on('data', function (file) {
-        assert.strictEqual(file.contents.toString(), expected);
-        assert.strictEqual(file.path.slice(file.path.indexOf('.')), '.json');
-    });
+describe("Given we are using gulp-csvtojson",function(){
+    describe("Given csv data",function(){
+        var csvData = "Header1,Header2,Header3\n"+
+                        "col1-row1,col2-row1,col3-row1\n"+
+                        "col1-row2,col2-row2,col3-row2";
+        
+        describe("When using a Buffer",function(){
+            var file = null;
+            var file2 = null;
+            beforeEach(function(){
+                file = new File({
+                    path : 'any.csv',
+                    contents : new Buffer(csvData)
+                });
+                file2 = new File({
+                    path : 'file.csv',
+                    contents : new Buffer(input)
+                });
+            });
+            describe("When using default options",function(){
+                var csvtojsonStream = null;
+                beforeEach(function(){
+                    csvtojsonStream = gulpCsvtojson();
+                });
 
-    stream.on('end', cb);
+                it("should generate expected json string",function(done){
+                    var expectedResult = JSON.stringify([{"Header1":"col1-row1","Header2":"col2-row1","Header3":"col3-row1"},{"Header1":"col1-row2","Header2":"col2-row2","Header3":"col3-row2"}]);
+                    
+                    csvtojsonStream.once('data',function(file){
+                        assert(file.contents.toString() == expectedResult);
+                        done();
+                    });
+                    csvtojsonStream.write(file);
+                    csvtojsonStream.end();
+                });
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.csv',
-        contents: new Buffer(input)
-    }));
+                it('should return json when no options', function (done) {
+                    csvtojsonStream.once('data',function(file){
+                        assert(file.contents.toString() == expected);
+                        done();
+                    });
 
-    stream.end();
-});
+                    csvtojsonStream.write(file2);
+                    csvtojsonStream.end();
+                });
 
-it('should return js when globalvariable is set null', function (cb) {
-    var stream = csvtojson({ globalvariable: null, genjs: true });
+                it("should rename path to a json file",function(done){
+                    csvtojsonStream.on('data',function(file){
+                        assert.equal(file.relative,'any.json');
+                        done();
+                    });
 
-    stream.on('data', function (file) {
-        assert.strictEqual(file.contents.toString(), "file=" + expected + ";");
-        assert.strictEqual(file.path.slice(file.path.indexOf('.')), '.js');
-    });
+                    csvtojsonStream.write(file);
+                    csvtojsonStream.end();
+                });
+            });
+            describe("When using generate js options",function(){
+                var csvtojsonStream = null;
+                beforeEach(function(){
+                    csvtojsonStream = gulpCsvtojson({ globalvariable: null, genjs: true });
+                });
 
-    stream.on('end', cb);
+                it("should return js when globalvariable is set null",function(done){                    
+                    csvtojsonStream.once('data',function(file){
+                        assert(file.contents.toString(), "file=" + expected + ";");
+                        done();
+                    });
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.csv',
-        contents: new Buffer(input)
-    }));
+                    csvtojsonStream.write(file2);
+                    csvtojsonStream.end();
+                });
 
-    stream.end();
-});
+            });
+            describe("When setting globalvariable option without genjs",function(){
+                var csvtojsonStream = null;
+                beforeEach(function(){
+                    csvtojsonStream = gulpCsvtojson({ globalvariable: 'gv'});
+                });
 
-it('should return json when globalvariable is set without set genjs', function (cb) {
-    var stream = csvtojson({ globalvariable: 'gv' });
+                it("should return js when globalvariable is set null",function(done){                    
+                    csvtojsonStream.once('data',function(file){
+                        assert(file.contents.toString(),  expected );
+                        done();
+                    });
 
-    stream.on('data', function (file) {
-        assert.strictEqual(file.contents.toString(), expected);
-        assert.strictEqual(file.path.slice(file.path.indexOf('.')), '.json');
-    });
+                    csvtojsonStream.write(file2);
+                    csvtojsonStream.end();
+                });
 
-    stream.on('end', cb);
+            });
+            describe("When setting globalvariable option with genjs",function(){
+                var csvtojsonStream = null;
+                beforeEach(function(){
+                    csvtojsonStream = gulpCsvtojson({ globalvariable: 'gv', genjs: true });
+                });
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.csv',
-        contents: new Buffer(input)
-    }));
+                it("should return js when globalvariable is set null",function(done){                    
+                    csvtojsonStream.once('data',function(file){
+                        assert(file.contents.toString(),  "gv=" + expected + ";" );
+                        done();
+                    });
 
-    stream.end();
-});
+                    csvtojsonStream.write(file2);
+                    csvtojsonStream.end();
+                });
 
-it('should return js when globalvariable is set', function (cb) {
-    var stream = csvtojson({ globalvariable: 'gv', genjs: true });
+            });
+        });
 
-    stream.on('data', function (file) {
-        assert.strictEqual(file.contents.toString(), "gv=" + expected + ";");
-        assert.strictEqual(file.path.slice(file.path.indexOf('.')), '.js');
-    });
+        describe("When using a Stream",function(){
+            var file = null;
+            beforeEach(function(){
+                file = new File({
+                    path : 'any.csv',
+                    contents : stringToStream(csvData)
+                });
+            });
+            describe("When using default options",function(){
+                var csvtojsonStream = null;
+                beforeEach(function(){
+                    csvtojsonStream = gulpCsvtojson();
+                });
 
-    stream.on('end', cb);
+                it("should generate expected json string",function(done){
+                    var expectedResult = [{"Header1":"col1-row1","Header2":"col2-row1","Header3":"col3-row1"},{"Header1":"col1-row2","Header2":"col2-row2","Header3":"col3-row2"}];
+                    csvtojsonStream.on('data',function(file){
+                        file.contents.pipe(concat(function(data){
+                            assert(JSON.stringify(JSON.parse(data.toString())) == JSON.stringify(expectedResult));
+                            done();
+                        }));
+                    });
+                    csvtojsonStream.write(file);
+                    csvtojsonStream.end();
+                });
+                it("should rename path to a json file",function(done){
+                    csvtojsonStream.on('data',function(file){
+                        assert.equal(file.relative,'any.json');
+                        done();
+                    });
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.csv',
-        contents: new Buffer(input)
-    }));
+                    csvtojsonStream.write(file);
+                    csvtojsonStream.end();
+                });
+            });
 
-    stream.end();
+            describe("When defining generate js option",function(){
+                var csvtojsonStream = null;
+                beforeEach(function(){
+                    csvtojsonStream = gulpCsvtojson({genjs:true});
+                });
+
+                it("should generate expected json string",function(done){
+
+                    var expectedResult = 'var any = [{"Header1":"col1-row1","Header2":"col2-row1","Header3":"col3-row1"},{"Header1":"col1-row2","Header2":"col2-row2","Header3":"col3-row2"}];';
+                    csvtojsonStream.on('data',function(file){
+                        file.contents.pipe(concat(function(data){
+                            assert(data.toString().replace(/\s\n/g,'') == expectedResult);
+                            done();
+                        }));
+                    });
+                    csvtojsonStream.write(file);
+                    csvtojsonStream.end();
+                });
+                it("should rename path to a js file",function(done){
+                    csvtojsonStream.on('data',function(file){
+                        assert.equal(file.relative,'any.js');
+                        done();
+                    });
+
+                    csvtojsonStream.write(file);
+                    csvtojsonStream.end();
+                });
+            }); 
+        });                        
+    })
 });
